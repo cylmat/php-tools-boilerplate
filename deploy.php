dd052c9fbfbeb260073cd6a92717c13b60e6e7a8
cylmat <cyrilmatte.pro@gmail.com>
2021-03-10 15:49:50 +0000

    Repo

--- deploy.php
+++ deploy.php
@@ -3,17 +3,47 @@ namespace Deployer;
 
 require 'recipe/common.php';
 
+/**
+ * Deployer
+ * https://deployer.org
+ */
+/* 
+ * Will create on host:
+ *   releases/ contains releases dirs,
+ *   share/    contains shared files and dirs,
+ *   current/  symlink to current release.
+ */
+
+/**
+ * CLI usage
+ *
+ * dep deploy
+ * dep rollback
+ * dep run '<shell_command>'
+ * dep ssh (connect to host)
+ * 
+ * dep help deploy
+ */
+
+/*********
+ * Params
+ */
+
 // Project name
 set('application', 'my_sample_project');
-set('application_directory', '/tmp/deployer/');
+set('target_directory', '/tmp/deployer/');
 
 // Project repository
 // export DEPLOYER_REPOSITORY=<http://my_repository>
 if (!isset($_ENV['DEPLOYER_REPOSITORY'])) {
+    echo 'export DEPLOYER_REPOSITORY="http://<vsc>/<vendor>/<repo>.git" must be set.'."\n";
     exit(1);
 }
 
 set('repository', $_ENV['DEPLOYER_REPOSITORY']);
+
+// keeps the last 5 releases by default
+set('keep_releases', 5);
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
@@ -26,19 +56,28 @@ set('shared_dirs', []);
 set('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
-/** 
+/********
  * Hosts
  */
 
 # host('http://sample.host')
 #    ->set('deploy_path', '/var/www/var/deployer/{{application}}');    
-set('deploy_path', '{{application_directory}}{{application}}');  
+set('deploy_path', '{{target_directory}}{{application}}');  
 
-/**
+/********
  * Tasks
  */
 
+// Config task //
+
+/*task('my_task', function () {
+    $param = get('my_param');
+});*/
+
+// Run tasks //
+
 desc('Deploy your project');
+
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
@@ -55,5 +94,10 @@ task('deploy', [
     'success'
 ]);
 
+/*********
+ * Orders
+ */
+
 // [Optional] If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
+after('cleanup', 'success');
