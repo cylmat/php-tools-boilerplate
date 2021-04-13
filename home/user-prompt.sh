cf1544185b063d58c8f57233b5477b48d8010d69
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-13 21:38:22 +0200

    Update user-prompt.sh

--- home/user-prompt.sh
+++ home/user-prompt.sh
@@ -2,23 +2,24 @@
 
 ############
 # GIT PROMPT
-# https://thucnc.medium.com/how-to-show-current-git-branch-with-colors-in-bash-prompt-380d05a24745
 ############
 
 # https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 # https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 
 ### CHANGE PROMPT
-if [[ ! -f ~/prompt/git-prompt.sh ]] || [[ ! -f ~/prompt/git-completion.bash ]]
-then
-  echo "git-prompt or git-completion not found"
+if [[ ! -f ~/prompt/git-prompt.sh ]] || [[ ! -f ~/prompt/git-completion.bash ]]; then
+  echo "git-prompt or git-completion not found!"
   return 1
-else
-  echo "git prompt & completion found..."
 fi
 
-parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
+source ~/prompt/git-completion.bash
+source ~/prompt/git-prompt.sh
 
-export PS1="\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\]$ "
+# CHOOSE ONE PROMPT FILE
+CUSTOM_PROMPT=default.sh
+
+if [[ -f ~/prompt/$CUSTOM_PROMPT ]]; then
+  source ~/prompt/$CUSTOM_PROMPT
+  echo "Prompt to $CUSTOM_PROMPT"
+fi
