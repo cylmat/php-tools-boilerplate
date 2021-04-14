64c7d845cd010613708da266dc108a8a35cc2cd4
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-14 21:39:33 +0200

    Update fancygit.sh

--- home/prompt/fancygit.sh
+++ home/prompt/fancygit.sh
@@ -7,10 +7,16 @@
 
 if [[ ! -d $HOME/.fancy-git ]]; then
   echo "Fancygit not installed in $HOME/.fancy-git!"
-  echo 'Please "curl -sS https://raw.githubusercontent.com/diogocavilha/fancy-git/master/install.sh | sh"' 
+  echo "Please install fontconfig"
+  echo 'And "curl -sS https://raw.githubusercontent.com/diogocavilha/fancy-git/master/install.sh | bash"'
+  return
 fi;
 
 fancygit configure-fonts
+# simple, human-single-line, human-dark-single-line, etc... 
+# then "source ~/.fancy-git/prompt.sh" again
 fancygit simple
+
+source ~/.fancy-git/prompt.sh
 
 echo "Fancygit simple setted."
