7d7561868db34652f86aa4388434ee2a19798a8f
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-04-14 21:38:45 +0200

    Update user-prompt.sh

--- home/user-prompt.sh
+++ home/user-prompt.sh
@@ -16,10 +16,25 @@ fi
 source ~/prompt/git-completion.bash
 source ~/prompt/git-prompt.sh
 
-# CHOOSE ONE PROMPT FILE
-CUSTOM_PROMPT=default.sh
+##########################
+# CHOOSE ONE PROMPT FILE #
+# default, fancygit, git, informative, matthew
+##########################
 
-if [[ -f ~/prompt/$CUSTOM_PROMPT ]]; then
-  source ~/prompt/$CUSTOM_PROMPT
-  echo "Prompt to $CUSTOM_PROMPT"
+# Set your own custom prompt file
+SET_CUSTOM_PROMPT="fancygit"
+if [[ $SET_CUSTOM_PROMPT != 'default' ]]; then
+
+  CUSTOM_PROMPT="${SET_CUSTOM_PROMPT}.sh"
+
+  if [[ -f ~/prompt/$CUSTOM_PROMPT ]]; then
+    export PROMPT_COMMAND=''
+    source ~/prompt/$CUSTOM_PROMPT
+    # echo "Prompt to $CUSTOM_PROMPT"
+  else 
+    echo "~/prompt/$CUSTOM_PROMPT not found!"
+  fi
+else
+  export PROMPT_COMMAND=''
+  export PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 fi
